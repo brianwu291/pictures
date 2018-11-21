f3a48b89207b3ce2cf115fc88960990db51882a4
brianwu <38502299+brianwu291@users.noreply.github.com>
2018-11-21 16:20:53 +0800

    Updates

---
+++ precache-manifest.c2c6b36030aa39a0c81d404eb70f20b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97c2b9a4a4aca29efa59",
+    "url": "/pictures/static/js/runtime~main.97c2b9a4.js"
+  },
+  {
+    "revision": "a0e3a392f7b09631ca51",
+    "url": "/pictures/static/js/main.a0e3a392.chunk.js"
+  },
+  {
+    "revision": "26387354318e7d868eeb",
+    "url": "/pictures/static/js/1.26387354.chunk.js"
+  },
+  {
+    "revision": "a0e3a392f7b09631ca51",
+    "url": "/pictures/static/css/main.c42aaab9.chunk.css"
+  },
+  {
+    "revision": "6d89176b35f73ce0e5a91bcb347138ed",
+    "url": "/pictures/index.html"
+  }
+];
